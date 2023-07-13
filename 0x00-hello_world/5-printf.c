22147b122379ac47960efc1ebcdddde0f9cecc4f
AMRBY <bouyoussefamr@gmail.com>
2023-07-13 17:14:15 +0000

    mandatory

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - printf string
+*Description: "with proper grammar, but the outcome is a piece of art,"
+*Return: value 0
+*/
+int main(void)
+{
+	char x[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n",x);
+	return (0);
+}
