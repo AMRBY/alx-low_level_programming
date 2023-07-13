1903b3cc4606539b1fe83f2bf8d298d4020ba274
AMRBY <bouyoussefamr@gmail.com>
2023-07-13 23:40:29 +0000

    mandatory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 /**
-*main - display string
-*Description: "Programming is like building a multilingual puzzle"
-*Return: value 0
-*/
+ *  * main - prints the size of various types.
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+
 int main(void)
 {
-	printf("Size of an char: %i byte(s)\n", sizeof(char));
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
-	printf("Size of an long int: %i byte(s)\n", sizeof(long int));
-	printf("Size of an long long int: %i byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %i byte(s)\n", sizeof(float));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+
 	return (0);
 }
+
