d1af46105bf8f32076034fe587b8d461e42ac46c
AMRBY <bouyoussefamr@gmail.com>
2023-07-13 17:47:03 +0000

    mandatory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - display string
+*Description: "Programming is like building a multilingual puzzle"
+*Return: value 0
+*/
+int main(void)
+{
+	printf("Size of an char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of an long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of an long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of an float: %li byte(s)\n", sizeof(float));
+	return (0);
+}
