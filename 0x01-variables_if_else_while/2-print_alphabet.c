d6fa3857d7f02f2d56dd5d790953232c95b88c19
AMRBY <bouyoussefamr@gmail.com>
2023-07-14 16:45:49 +0000

    mandatory

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,13 @@
 * followed by a new line
 * Return: Always 0 (Success)
 */
-int main(void){
-	char ch;
+int main(void)
+{
+	char c;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-	putchar(ch);
+	putchar(c);
 	}
 	putchar('\n');
 	return (0);
