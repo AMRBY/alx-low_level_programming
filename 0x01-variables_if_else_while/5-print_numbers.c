69c76efe255f7149da05f8378d1b7f9bd2b98dc8
AMRBY <bouyoussefamr@gmail.com>
2023-07-14 16:59:31 +0000

    mandatory

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+	printf("%d\n", c);
+	}
+	return (0);
+}
