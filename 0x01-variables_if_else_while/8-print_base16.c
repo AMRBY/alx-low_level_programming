8b5e6f7b50106654b70db4ac15b6531a2378ff60
AMRBY <bouyoussefamr@gmail.com>
2023-07-14 17:26:14 +0000

    mandatory

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+	putchar(c);
+	}
+	for (c = 97; c <= 102; c++)
+        {
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
