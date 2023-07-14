55fdc59f954daa4d89c9b53c2d73cd62eb321c11
AMRBY <bouyoussefamr@gmail.com>
2023-07-14 21:36:24 +0000

    mandatory

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase,
+* followed by a new line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c = 48;
+	int v = 48;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (; v <= 57; v++)
+		{
+		  if (c != v)
+		  {
+			putchar(c);
+			putchar(v);
+		  	if (c != 56 || v != 57)
+		  	{	
+				putchar(',');
+				putchar(' ');
+		  	}
+		  }
+		}
+	v = c + 1;
+	}
+putchar('\n');
+return (0);
+}
