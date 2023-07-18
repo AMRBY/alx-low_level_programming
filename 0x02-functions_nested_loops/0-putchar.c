fc784f96c242fe9c4e3464e5199182019e7e0e02
AMRBY <bouyoussefamr@gmail.com>
2023-07-18 18:18:23 +0000

    mandatory

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main - putchar
  * Description: display _putchar string
  * Return: 0
 */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 int main (void){
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
