1a09721b14213a66f9993ac333f5e504b2200c61
AMRBY <bouyoussefamr@gmail.com>
2023-07-18 19:23:46 +0000

    mandatory

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - putchar
+ * Description: display _putchar string
+ * Return: 0
+*/
+int main(void)
+{
+int i;
+
+	for (i = 97; i < 123; i++){
+		_putchar(i);
+	}
+	_putchar('\n');
+
+return (0);
+}
