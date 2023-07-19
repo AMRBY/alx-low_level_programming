e09fda005f3513a1efabdfc1371f8b27a9c03e67
AMRBY <bouyoussefamr@gmail.com>
2023-07-19 00:18:18 +0000

    mandatory

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints the last digit of a number
+ * @i: integer
+ * @j: integer
+ * Description: prints the last digit of a number
+ * Return: integer
+ */
+
+int add(int i, int j)
+{
+	int z;
+
+	z = i + j;
+return (z);
+}
