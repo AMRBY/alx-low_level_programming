7afbfe3f41b6819f6233e4d6686e0ac7e686328e
AMRBY <bouyoussefamr@gmail.com>
2023-07-20 14:23:50 +0000

    mandatory

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - find digit
+ * Description: find digit
+ * Return: 0/1
+ **/
+int main(void)
+{
+	int i = 2;
+	long j = 612852475143;
+
+	while (i < j)
+	{
+		while (j % i != 0)
+		{
+			i++;
+		}
+		j = j / i;
+	}
+	printf("<%d>\n", i);
+	return (0);
+}
