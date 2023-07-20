babf294913ed5e4c741c0dfa906e8b26be1e7e1a
AMRBY <bouyoussefamr@gmail.com>
2023-07-20 06:21:03 +0000

    mandatory

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - find digit
+ * Description: find digit
+ * Return: 0/1
+ * */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - find digit
+ * Description: find digit
+ * Return: 0/1
+ * */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
