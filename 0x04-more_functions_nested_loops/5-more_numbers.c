a3d8947daa5dd5d852406b05d1f53dee95f22302
AMRBY <bouyoussefamr@gmail.com>
2023-07-20 06:52:41 +0000

    mandatory

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,25 @@
  * */
 void more_numbers(void)
 {
-	int i;
-	int j = 58;
+	int i, z, j;
 
-	for (i = 48; i < j; i++)
+	for (z =0; z < 10; z++)
 	{
-		_putchar(i);
-		if (i == 57)
+		j = 58;
+		for (i = 48; i < j; i++)
 		{
-			j = 53;
-			i = 47;
+			_putchar(i);
+			if (i == 57)
+			{
+				j = 53;
+				i = 47;
+			}
+			if (j == 53 && i < 52)
+			{
+				_putchar('1');
+			}
 		}
-		if (j == 53 && i < 52)
-		{
-			_putchar('1');
-		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 	return;
 }
