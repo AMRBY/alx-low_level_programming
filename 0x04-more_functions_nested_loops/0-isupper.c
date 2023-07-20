3f68db8236512659c651cfef5fba13bd85b08ace
AMRBY <bouyoussefamr@gmail.com>
2023-07-20 05:29:30 +0000

    mandatory

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+
+/**
+ * main - find uppercase
+ * Description: find uppercase
+ * Return: 0
+ * */
+
+int _isupper(int c){
+	
+	c = isupper(c);
+	if (c != 0)
+		return (1);
+	else
+		return (0);
+}
