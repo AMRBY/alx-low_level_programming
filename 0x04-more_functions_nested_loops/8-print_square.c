da90ad1c4c259395ed89f069ce165a7040a0a54b
AMRBY <bouyoussefamr@gmail.com>
2023-07-20 07:21:42 +0000

    mandatory

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - find digit
+ * Description: find digit
+ * Return: 0/1
+ * */
+void print_square(int size)
+{
+	int i;
+	int j =0;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			j = 0;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+	return;
+}
