fbd37487d2fbb0c86c39d398ba73e0532963dc52
AMRBY <bouyoussefamr@gmail.com>
2023-07-24 16:17:45 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - 98
+ * Description: find digit
+ * @a: int pointer
+ * @b : int pointer
+ * Return: no
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
