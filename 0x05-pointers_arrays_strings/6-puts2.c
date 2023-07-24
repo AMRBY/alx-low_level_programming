7e6618668280d64e674e5d960d59daa1cda05c02
AMRBY <bouyoussefamr@gmail.com>
2023-07-24 20:33:10 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - length of strings
+ * Description: find digit
+ * @str: int pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i % 2 == 0) 
+		{
+			_putchar(str[i]);
+			
+		}
+		i++;
+	}
+	_putchar('\n');
+}
+
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,11 +7,9 @@
  */
 int main(void)
 {
-	char s[11] = "Holberton!";
-    //char s[446] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    char *str;
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
+    str = "0123456789";
+    puts2(str);
     return (0);
 }
