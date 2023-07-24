bfd5bf5db4ce3df143ba71deb6116887e17699ee
AMRBY <bouyoussefamr@gmail.com>
2023-07-24 22:29:35 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - length of strings
+ * Description: find digit
+ * @a: int pointer
+ * @n: int variable
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n-1)
+	{
+		printf("%d, ",a[i]);
+		i++;
+	}
+	printf("%d\n",a[n-1]);
+}
+
