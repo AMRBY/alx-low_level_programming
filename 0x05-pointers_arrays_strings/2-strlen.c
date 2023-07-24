9de72a44c593f85bdb441ca9b395c4d2a3e86207
AMRBY <bouyoussefamr@gmail.com>
2023-07-24 16:36:41 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * strlen - 98
+ * Description: find digit
+ * @s: int pointer
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int v;
+
+	v = strlen(s);
+	return (v);
+}
+
