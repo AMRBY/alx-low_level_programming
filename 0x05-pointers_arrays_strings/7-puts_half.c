e4f67daeef0809031f1932dd50681c7d84ba09c4
AMRBY <bouyoussefamr@gmail.com>
2023-07-24 20:49:54 +0000

    mandatory

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - length of strings
+ * Description: find digit
+ * @str: int pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len;
+
+	while (str[i])
+	{
+		i++;
+	}
+	len = i / 2;
+	while (len < i )
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
+
