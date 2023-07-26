a89d77644790ac3f40d8cb9068a648d90f896de3
AMRBY <bouyoussefamr@gmail.com>
2023-07-26 13:56:58 +0000

    mandatory

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - length of strings
+ * Description: find digit
+ * @src: char pointer
+ * @dest: char pointer
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *x;
+
+	x = strcat(dest, src);	
+	return (x);
+}
+
+
