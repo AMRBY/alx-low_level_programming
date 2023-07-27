0342b955ca84414e746d2d8af48828fafcf5af8e
AMRBY <bouyoussefamr@gmail.com>
2023-07-27 22:19:41 +0000

    mandatory

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rot13 - length of strings
+ * Description: find digit
+ * @p: pointer
+ * Return: char
+ */
+char *rot13(char *p)
+{
+	int i = 0;
+	int j = 0;
+	char a[] = "abcdefghijklmABCDEFGHIJKLM";
+	char z[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		j = 0;
+		while (a[j] != '\0')
+		{
+			if (p[i] == a[j])
+				p[i] = z[j];
+			else if (p[i] == z[j])
+				p[i] = a[j];
+		j++;
+		}
+	}
+	return (p);
+}
