953b45f6f84045f7488b7a013362455c31c8c493
AMRBY <bouyoussefamr@gmail.com>
2023-07-27 19:05:43 +0000

    mandatory

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_toupper - length of strings
+ * Description: find digit
+ * Return: char
+ */
+void *string_toupper(char *p)
+{
+	int i = 0;
+
+	while (p[i] != '\0')
+	{
+		if (p[i] > 96 && p[i] < 123)
+		{
+	 	p[i] = p[i] - 32;
+		}
+		i++;
+	}
+	return (p);
+}
