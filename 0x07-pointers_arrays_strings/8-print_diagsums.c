4a70a281f363cd564c15f299c2c632aa2659a306
AMRBY <bouyoussefamr@gmail.com>
2023-07-31 23:25:08 +0000

    mandatory

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,14 +11,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int j;
 	int s1 = 0;
 	int s2 = 0;
 
 		for (j = 0; j < size; j++)
 		{
-			s1 += a[(size * i) + i];
-			s2 += a[(size * (i + 1)) - (i + 1)];
+			s1 += a[(size * j) + j];
+			s2 += a[(size * (j + 1)) - (j + 1)];
 		}
 	printf("%d, %d\n", s1, s2);
 }
