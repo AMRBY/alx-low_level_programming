d3b1f071ce49b7a5ddaeb59f2ff90397d03f1275
AMRBY <bouyoussefamr@gmail.com>
2023-08-02 19:06:07 +0000

    mandatory

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - length of strings
+ * Description: find digit
+ * @s: char pointer
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+        return (0);
+}
