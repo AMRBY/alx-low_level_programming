63026f7589d0ef01b8f4376de1c17dd3c2feb608
AMRBY <bouyoussefamr@gmail.com>
2023-08-02 23:46:01 +0000

    mandatory

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * factorial - length of strings
+ * Description: find digit
+ * @n: char pointer
+ * Return: void
+ */
+int factorial(int n)
+{
+	int i = 0;
+	int x = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		x = n * factorial(n - 1);
+		return (x);
+	}
+	else
+		return(1);
+}
