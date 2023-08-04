9955fdf73d1a29f0f7e3c53ab60b387e215add54
AMRBY <bouyoussefamr@gmail.com>
2023-08-04 19:19:27 +0000

    mandatory

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function
+* Description: its name
+* @argv: argv
+* @argc: argc
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
