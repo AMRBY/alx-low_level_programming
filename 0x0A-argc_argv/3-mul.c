eaeba7f6a1c94a79cc2f9a685bf42304ed72b49c
AMRBY <bouyoussefamr@gmail.com>
2023-08-04 22:23:21 +0000

    mandatory

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - function
+* Description: its name
+* @argv: argv
+* @argc: argc
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc > 2)
+	{
+		x = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", x);
+		return(0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
