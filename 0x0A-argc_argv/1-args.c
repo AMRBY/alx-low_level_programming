68227832439dcaef893b5491237e34d3cae3f1b6
AMRBY <bouyoussefamr@gmail.com>
2023-08-04 19:30:58 +0000

    mandatory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - function
+* Description: its name
+* @argv: argv
+* @argc: argc
+* Return: 0
+*/
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
