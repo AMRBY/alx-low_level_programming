755db445af1fab400c68aa606d2b6c528b1331b3
AMRBY <bouyoussefamr@gmail.com>
2023-08-08 20:13:16 +0000

    mandatory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,20 +17,21 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;
 	int z = 0;
 	int y = 0;
-
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
+	if (s1 != NULL)
+		while (s1[i])
+			i++;
+	if (s2 != NULL)
+		while (s2[j])
+			j++;
 	a = malloc(sizeof(char) * (i + j + 1));
 	if (a == NULL)
 		return (NULL);
-	while (z < i && *s1 != NULL)
+	while (z < i)
 	{
 		a[z] = s1[z];
 		z++;
 	}
-	while (y < j && *s2 != NULL)
+	while (y < j)
 	{
 		a[z] = s2[y];
 		z++;
