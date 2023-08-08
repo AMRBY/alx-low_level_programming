211ee63c5bc54e4f357a5a79611ac3918c448637
AMRBY <bouyoussefamr@gmail.com>
2023-08-08 17:25:55 +0000

    mandatory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup  - find uppercase
+ * Description: find uppercase
+ * @str : arg
+ * Return: a
+ */
+
+char *_strdup(char *str)
+{
+	char *a;
+	int i = 0;
+	int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\n')
+		i++;
+	a = malloc(sizeof(str) * i);
+	if (a == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		a[j] = str[j];
+		j++;
+	}
+	return (a);
+}
