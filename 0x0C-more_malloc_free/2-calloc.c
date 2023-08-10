c5e515b71ea2fae43164373c5128ad91c4635e61
AMRBY <bouyoussefamr@gmail.com>
2023-08-10 16:39:18 +0000

    mandatory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc  - find uppercase
+ * Description: find uppercase
+ * @nmemb : arg
+ * @size : arg
+ * Return: a
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *a;
+	char *b;
+	unsigned int z = 0;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	a = malloc(nmemb * size);
+
+	if (a == NULL)
+		return (NULL);
+
+	b = a;
+	while (z < nmemb)
+	{
+		b[z] = '\0';
+		z++;
+	}
+	return (a);
+}
