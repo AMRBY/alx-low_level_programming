af9e82d13745520e3603681e63981179e64c6f32
AMRBY <bouyoussefamr@gmail.com>
2023-08-10 18:36:06 +0000

    mandatory

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range  - find uppercase
+ * Description: find uppercase
+ * @min : arg
+ * @max : arg
+ * Return: a
+ */
+
+int *array_range(int min, int max)
+{
+	int *a;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+
+	a = malloc(sizeof(int) * (max - min + 1));
+
+	if (a == NULL)
+		return (NULL);
+	for (i = min; i < max - min + 1; i++)
+	{
+		a[j++] = i;
+	}
+	return (a);
+}
