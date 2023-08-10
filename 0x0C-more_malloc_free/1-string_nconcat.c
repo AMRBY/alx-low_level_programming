da6004527857f1bf8e44a0667ab8410c8f3715ec
AMRBY <bouyoussefamr@gmail.com>
2023-08-10 15:21:03 +0000

    mandatory

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,10 +20,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int y = 0;
 	int m = 0;
 
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
+	if (s1 != NULL)
+		while (s1[i])
+			i++;
+	if (s2 != NULL)
+		while (s2[j])
+			j++;
 	if (n >= j)
 		m = j;
 	a = malloc(sizeof(char) * i + sizeof(char) * m);
