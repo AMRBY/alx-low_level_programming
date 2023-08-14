bc335fd5051c819146565665bb9aafa434dc2048
AMRBY <bouyoussefamr@gmail.com>
2023-08-14 22:20:48 +0000

    mandatory

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+/**
+ * init_dog - length of strings
+ * Description: find digit
+ * @d: char pointer
+ * @name: char pointer
+ * @owner: char pointer
+ * @age: char pointer
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+}
+
+
