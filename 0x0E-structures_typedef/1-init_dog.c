1e528c9597bfd65fd57b7fa122f2e53a3b20ee6d
AMRBY <bouyoussefamr@gmail.com>
2023-08-14 22:59:43 +0000

    mandatory

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,8 +5,8 @@
  * Description: find digit
  * @d: char pointer
  * @name: char pointer
- * @owner: char pointer
  * @age: char pointer
+ * @owner: char pointer
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
