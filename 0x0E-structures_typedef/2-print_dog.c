f88425ff5880a5f553497cc40e12440f38603453
AMRBY <bouyoussefamr@gmail.com>
2023-08-14 23:42:59 +0000

    mandatory

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,17 +11,9 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	else
-		if (d->name == "NULL")
-			d->name = "nil";
-		if (! (d->age))
-			d->age = 0;
-		if (d->owner == "NULL")
-			d->owner = "nil";
-
-//d = malloc(sizeof(struct dog));
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	//d->name = name;
-	//d->age = age;
-	//d->owner = owner;
+	if (d->name == "NULL")
+		d->name = "nil";
+	if (d->owner == "NULL")
+		d->owner = "nil";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
