8eb4b4b45a7666b127c6e8e9934ee9e21fe1a5ba
AMRBY <bouyoussefamr@gmail.com>
2023-08-14 22:44:10 +0000

    mandatory

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
+	if (!d)
+		return;
+
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
-	}
-	else
-		return;
 }
