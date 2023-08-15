1f5fa4cc3d94424c73f86df92004c12d00546b82
AMRBY <bouyoussefamr@gmail.com>
2023-08-15 00:49:54 +0000

    mandatory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - length of strings
+ * Description: find digit
+ * @d: char pointer
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d);
+}
