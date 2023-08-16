f9f2c95a5d4e6658609cf532ffe314c92676666a
AMRBY <bouyoussefamr@gmail.com>
2023-08-16 17:58:05 +0000

    mandatory

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* array_iterator - function
+* Description: its name
+* @array: argv
+* @size: argc
+* @action: argc
+* Return: void
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++) 
+		action(array[i]);
+}
