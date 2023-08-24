7f16269ca4e0b213bf54a254ff89a36bf8e1c9ae
AMR BY <bouyoussefamr@gmail.com>
2023-08-24 21:47:28 +0000

    mandatory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,7 +10,7 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
-	int i = 0;
+	size_t i = 0;
 
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
@@ -22,8 +22,7 @@ list_t *add_node(list_t **head, const char *str)
 	node->str = strdup(str);
 	node->len = i;
 	node->next = *head;
+	*head = node;
 
-	printf("[%d] %s\n", node->len, node->str);
-
-	return (node->next);
+	return (*head);
 }
