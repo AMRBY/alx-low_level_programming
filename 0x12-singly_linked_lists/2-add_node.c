b1ddf04ca1264838451ba454c416e07035dcc66b
AMR BY <bouyoussefamr@gmail.com>
2023-08-24 21:54:03 +0000

    mandatory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,9 +16,6 @@ list_t *add_node(list_t **head, const char *str)
 	if (node == NULL)
 		return (NULL);
 
-	//while (str[i] != '\0')
-	//	i++;
-
 	node->str = strdup(str);
 	node->len = strlen(str);
 	node->next = *head;
