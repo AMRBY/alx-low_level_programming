38fdceb655309182237546624a614b75c781e125
AMR BY <bouyoussefamr@gmail.com>
2023-08-24 20:24:53 +0000

    mandatory

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_list - length of strings
+ * Description: find digit
+ * @h: int pointer
+ * Return: size_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *head;
+
+	head = h;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (i);
+}
