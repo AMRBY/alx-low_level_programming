1149a80c8d01c8694e1403b68d64be7e426c8f52
AMR BY <bouyoussefamr@gmail.com>
2023-08-24 21:13:05 +0000

    mandatory

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,6 +12,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *h;
 	const char *z;
 
+	if (str == NULL || head == NULL)
+		return (NULL);
+
 	z = malloc(sizeof(char) * strlen(str));
 	if (z == NULL)
 		return (NULL);
