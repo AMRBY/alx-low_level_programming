f43524b66025e93cddc26bb6da94c4f3914fe741
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 15:03:10 +0000

    mandatory

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - length of strings
+ * Description: find digit
+ * @head: int pointer
+ * Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *h, *pr_h;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	h = *head;
+	*head = h->next;
+	n = h->n;
+	free(h);
+	return (n);
+}
