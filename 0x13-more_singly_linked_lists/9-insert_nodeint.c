522d3f9df95e78800cd3edcceb524a7dd1643e4c
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 20:28:47 +0000

    mandatory

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	h_pr = *head;
-	for (z = 0; z < idx; z++)
+	for (z = 1; z < idx; z++)
 	{
 		h_pr = h_pr->next;
 	}
