81482c4d6dcafca1d023e91ca00681c4ed6ca609
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 14:30:02 +0000

    mandatory

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - length of strings
+ * Description: find digit
+ * @head: int pointer
+ * Return: listint_t
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *h;
+
+	h = *head;
+	while (h != NULL)
+	{
+		*head = h->next;
+		free(h);
+		h = *head;
+	}
+	*head = NULL;
+
+}
