4052642c7560c5af036421d59f2c58ec8e45062c
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 20:38:00 +0000

    mandatory

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,5 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	h->n = n;
 	h->next = h_pr->next;
 	h_pr->next = h;
+	if (z < idx)
+		return (NULL);
 	return (h);
 }
