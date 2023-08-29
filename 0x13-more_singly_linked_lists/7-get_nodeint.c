b0ccfb18e2363984b25fc363dc2d454c3f434857
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 15:17:45 +0000

    mandatory

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - length of strings
+ * Description: find digit
+ * @head: int pointer
+ * @index: int pointer
+ * Return: int
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *h;
+	unsigned int n = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	h = head;
+	while (n < index)
+	{
+		head = h->next;
+		n++;
+		h = head;
+	}
+	return (h);
+}
