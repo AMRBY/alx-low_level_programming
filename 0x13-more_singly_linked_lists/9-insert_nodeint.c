70773f339010193a011da47c2cbac020aac6dca1
AMR BY <bouyoussefamr@gmail.com>
2023-08-29 20:27:14 +0000

    mandatory

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *hh, *h, *h_pr;
+	listint_t *h, *h_pr;
 	size_t z = 0;
 
 	if (*head == NULL)
@@ -19,15 +19,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	h = malloc(sizeof(listint_t));
 	if (h == NULL)
 		return (NULL);
-	hh = *head;
+
+	h_pr = *head;
 	for (z = 0; z < idx; z++)
 	{
-		h_pr = *head;
-		*head = h_pr->next;
+		h_pr = h_pr->next;
 	}
 	h->n = n;
 	h->next = h_pr->next;
 	h_pr->next = h;
-	*head = hh;
 	return (h);
 }
