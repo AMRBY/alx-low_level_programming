7ba5020679938c1331ba31b67356c87fadc87698
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 16:07:12 +0000

    mandatory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile - length of strings
  * Description: find digit
  * @filename: int pointer
- * @letters int pointer
+ * @letters: int pointer
  * Return: size_t
  */
 ssize_t read_textfile(const char *filename, size_t letters)
