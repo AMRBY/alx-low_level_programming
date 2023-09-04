7b7d9ea2ce63e49b2a21e5d5e995bf6f96080368
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 15:58:59 +0000

    mandatory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,45 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print
+ * read_textfile - length of strings
+ * Description: find digit
+ * @filename: int pointer
+ * @letters int pointer
+ * Return: size_t
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
+	int o;
+	ssize_t r, w;
+	char *p;
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	o = open(filename, O_RDONLY);
+	if (o == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+
+	p = malloc(sizeof(char) * letters);
+	if (p == NULL)
 	{
-		close(fd);
+		close(o);
 		return (0);
 	}
-	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
+	r = read(o, p, letters);
+	close(o);
+	if (r == -1)
 	{
-		free(buffer);
+		free(p);
 		return (0);
 	}
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-	if (lenr != lenw)
+
+	w = write(STDOUT_FILENO, p, r);
+	free(p);
+	if (w == -1)
 		return (0);
-	return (lenw);
+
+	if (r != w)
+		return (0);
+	return (w);
 }
