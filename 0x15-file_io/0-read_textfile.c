320c7f06bd90e07fc789526666ae600671447ad0
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 13:59:32 +0000

    mandatory

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * read_textfile - length of strings
+ * Description: find digit
+ * @filename: int pointer
+ * @letters int pointer
+ * Return: size_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	return (sum);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
 int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
 #endif
