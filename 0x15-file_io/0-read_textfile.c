991ded889e4c379fdce4fc53697869255a123c4f
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 15:47:20 +0000

    mandatory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,16 +17,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	o = open(filename, O_RDONLY);
 	if (o == -1)
-	{
 		return (0);
-	}
 
 	p = malloc(sizeof(char) * letters);
 	if (p == NULL)
+	{
+		close(o);
 		return (0);
+	}
 	r = read(o, p, letters);
 	if (r == -1)
 	{
+		close(o)
 		free(p);
 		return (0);
 	}
@@ -34,6 +36,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	w = write(1, p, r);
 	if (w == -1)
 	{
+		close(o);
 		free(p);
 		return (0);
 	}
