443bd4a2aec8d6a0bffab0a52f6450e7f8fb3be8
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 15:27:12 +0000

    mandatory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,6 +9,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int o, r, w;
+	char *p = malloc(sizeof(char) * letters);
 
-	return (sum);
+	if (filename == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, p, letters);
+	if (o == -1 || r == -1)
+		return (0);
+
+	w = write(1, p, r);
+	if (w == -1)
+		return (0);
+
+	close(o);
+	return (letters);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-int _putchar(char c);
+#include <unistd.h>
+#include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
