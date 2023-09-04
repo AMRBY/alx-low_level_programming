26cc68f97c22fb7959aae3442b9fd269a370fb40
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 15:55:11 +0000

    mandatory

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,44 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - length of strings
- * Description: find digit
- * @filename: int pointer
- * @letters int pointer
- * Return: size_t
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int o;
-	ssize_t r, w;
-	char *p;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	if (o == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	p = malloc(sizeof(char) * letters);
-	if (p == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		close(o);
+		close(fd);
 		return (0);
 	}
-	r = read(o, p, letters);
-	close(o);
-	if (r == -1)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
-		free(p);
+		free(buffer);
 		return (0);
 	}
-
-	w = write(1, p, r);
-	free(p);
-	if (w == -1)
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
 		return (0);
-
-	if (r != w)
-		return (0);
-	return (w);
+	return (lenw);
 }
