78a1b045ad44b9fc20a254a3d21f822f481ff385
AMR BY <bouyoussefamr@gmail.com>
2023-09-04 19:33:56 +0000

    mandatory

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,6 +19,9 @@ int create_file(const char *filename, char *text_content)
 	if (o == -1)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
+
 	len = strlen(text_content);
 	w = write(o, text_content, len);
 	if (w == -1)
