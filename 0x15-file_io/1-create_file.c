35aef56b040917712f790769ef27b95ef0fc63a4
AMRBY <bouyoussefamr@gmail.com>
2023-10-30 17:04:05 +0000

    mandatory

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - length of strings
+ * Description: find digit
+ * @filename: int pointer
+ * @text_content: int pointer
+ * Return: size_t
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (o == -1)
+		return (-1);
+	if (text_content)
+		len = strlen(text_content);
+
+	w = write(o, text_content, len);
+	if (w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
