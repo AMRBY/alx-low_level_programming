789de13747a5b099d4742e040c5a058b1294d448
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-08 22:04:57 +0000

    mandatory

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* print_dlistint - length of strings
+* Description: find digit
+* @h: int pointer
+* Return: size_t
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
+
