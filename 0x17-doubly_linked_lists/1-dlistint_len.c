65834565223f13e85c08e6128cea49e2496ebb51
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-08 22:07:20 +0000

    mandatory

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 /**
-* print_dlistint - length of strings
+* dlistint_len - length of strings
 * Description: find digit
 * @h: int pointer
 * Return: size_t
 */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
 
 	while (h != NULL)
 	{
-		printf("%i\n", h->n);
 		h = h->next;
 		n++;
 	}
