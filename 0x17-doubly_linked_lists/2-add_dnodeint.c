36c3f66823cd49170969c2f893eb47357fe0d92f
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-08 23:08:09 +0000

    mandatory

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+* add_dnodeint - length of strings
+* Description: find digit
+* @head: int pointer
+* @n: int
+* Return: size_t
+*/
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *h;
+
+	h = malloc(sizeof(dlistint_t));
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	h->n = n;
+	h->next = *head;
+	h->prev = NULL;
+
+	*head = h;
+	return (h);
+}
