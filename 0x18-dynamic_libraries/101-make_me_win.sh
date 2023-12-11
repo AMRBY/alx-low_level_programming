34a44021c80946de663a36fff3e42bef952ee60a
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-11 23:30:30 +0000

    mandatory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P . https://github.com/AMRBY/alx-low_level_programming/0x18-dynamic_libraries/libtest.so
+wget -P . https://github.com/AMRBY/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=libtest.so
