ec2ac0c36e0eb5cd6383b2a7ba4fc9f503f03f97
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-11 21:36:47 +0000

    mandatory

---
+++ 0x18-dynamic_libraries/100-mul.c
@@ -0,0 +1,49 @@
+#include <ctype.h>
+
+/**
+ * mul - find digit
+ * Description: find digit
+ * @a: int
+ * @b: int
+ * Return: 0/1
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+
+}
+int add(int a, int b)
+{
+        int c;
+
+        c = a + b;
+        return (c);
+
+}
+int sub(int a, int b)
+{
+        int c;
+
+        c = a - b;
+        return (c);
+
+}
+int div(int a, int b)
+{
+        int c;
+
+        c = a / b;
+        return (c);
+
+}
+int mod(int a, int b)
+{
+        int c;
+
+        c = a % b;
+        return (c);
+
+}
