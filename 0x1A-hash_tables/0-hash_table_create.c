38ad11f6ba306676e3c3d1d2ba25f29351c21cfb
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-12-22 23:03:36 +0000

    mandatory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *tab;
+	unsigned long int i = 0;
 
-	tab = malloc(sizeof(hash_table_t *));
+	tab = malloc(sizeof(hash_table_t ));
 	if (tab == NULL)
 		return (NULL);
 	tab->size = size;
-	tab->array = malloc(sizeof(hash_node_t **));
+	tab->array = malloc(size * sizeof(hash_node_t *));
 	if (tab->array == NULL)
 		return (NULL);
+	for (i = 0; i < tab->size; i++)
+		tab->array[i] = NULL;
 	return (tab);
 
 }
